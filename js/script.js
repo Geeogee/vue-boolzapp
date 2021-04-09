c724e35601b336c305b1b39b5d98a0c0c4550670
Gioele <gioele.armiento@gmail.com>
2021-04-09 15:52:14 +0200

    User list rendering and active chats working

--- js/script.js
+++ js/script.js
@@ -6,7 +6,120 @@ function initVue() {
 
         data: {
 
-            "test" : "Hello world!"
+            contacts: [
+
+                {
+                    "name" : "Amore",
+                    "avatar" : "img/amore.jpg",
+                    "messages" : [
+                        {
+                            "date" : "10/04/2021 15:30:55",
+                            "text" : "Hai portato a spasso il cane?",
+                            "status" : "sent"
+                        },
+
+                        {
+                            "date" : "10/04/2021 15:32:00",
+                            "text" : "Ricordati anche di dargli da mangiare!",
+                            "status" : "sent"
+                        },
+
+                        {
+                            "date" : "10/04/2021 15:50:00",
+                            "text" : "Si, tranquillo!",
+                            "status" : "received"
+                        }
+                    ]
+                },
+
+                {
+                    "name" : "Gino",
+                    "avatar" : "img/gino.jpg",
+                    "messages" : [
+                        {
+                            "date" : "10/04/2021 17:30:55",
+                            "text" : "Stasera quindi usciamo?",
+                            "status" : "sent"
+                        },
+
+                        {
+                            "date" : "10/04/2021 17:31:10",
+                            "text" : "Ci vediamo al bar.",
+                            "status" : "received"
+                        },
+
+                        {
+                            "date" : "10/04/2020 17:50:00",
+                            "text" : "Perfetto!",
+                            "status" : "received"
+                        }
+                    ]
+                },
+
+                {
+                    "name" : "Gino Ax",
+                    "avatar" : "img/gino-ax.jpg",
+                    "messages" : [
+                        {
+                            "date" : "10/04/2021 19:00:07",
+                            "text" : "Hai sentito Gino?",
+                            "status" : "received"
+                        },
+
+                        {
+                            "date" : "10/04/2021 19:15:10",
+                            "text" : "Siamo al Coppa, sbrigati.",
+                            "status" : "sent"
+                        },
+
+                        {
+                            "date" : "10/04/2020 19:16:00",
+                            "text" : "Scendo e arrivo! Ordinate anche per me!",
+                            "status" : "received"
+                        }
+                    ]
+                },
+
+                {
+                    "name" : "Mamma",
+                    "avatar" : "img/mamma.jpg",
+                    "messages" : [
+                        {
+                            "date" : "11/04/2021 9:00:00",
+                            "text" : "Stai ancora dormendo?",
+                            "status" : "received"
+                        },
+
+                        {
+                            "date" : "11/04/2021 9:00:10",
+                            "text" : "Chi dorme non piglia pesci!",
+                            "status" : "received"
+                        },
+
+                        {
+                            "date" : "11/04/2020 12:44:10",
+                            "text" : "Buongiorno!",
+                            "status" : "sent"
+                        }
+                    ]
+                },
+
+
+            ],
+
+            "activeChat" : ""
+        },
+
+        methods: {
+
+            showChat: function(index) {
+
+                const user = this.contacts[index];
+                const messages = user.messages;
+                this.activeChat = messages;
+                console.log("Active chat messages",this.activeChat)
+
+            }
         }
     })
 }
